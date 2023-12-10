574456ba65d292e09f4c581300ab9ac636988d08
Diego Salgado <dasalgadoc@unal.edu.co>
2023-12-10 13:22:07 -0500

    refactor element

--- internal/platform/gmail/gmail_scrap_source.go
+++ internal/platform/gmail/gmail_scrap_source.go
@@ -7,7 +7,14 @@ import (
 	"google.golang.org/api/gmail/v1"
 )
 
-const userId = "me"
+const (
+	sourceFile = "gmail_scrap_source.go"
+
+	userId          = "me"
+	attachmentQuery = "has:attachment"
+
+	subject = "Subject"
+)
 
 type ScrapSource struct {
 	service *gmail.Service
@@ -16,7 +23,7 @@ type ScrapSource struct {
 func NewGmailScrapSource() (*ScrapSource, *errors.ProjectError) {
 	srv, err := buildService()
 	if err != nil {
-		return nil, errors.NewProjectError("gmail_scrap_source.go", errors.BuilderError, err.Error())
+		return nil, errors.NewProjectError(sourceFile, errors.BuilderError, err.Error())
 	}
 	return &ScrapSource{
 		service: srv,
@@ -24,20 +31,19 @@ func NewGmailScrapSource() (*ScrapSource, *errors.ProjectError) {
 }
 
 func (s *ScrapSource) ListMessagesWithAttachments(criteria criteria.Criteria) *errors.ProjectError {
-	query := "has:attachment"
-	resp, err := s.service.Users.Messages.List(userId).Q(query).Do()
-
+	resp, err := s.service.Users.Messages.List(userId).Q(attachmentQuery).Do()
 	if err != nil {
-		return errors.NewProjectError("gmail_scrap_source.go", errors.ServiceError, err.Error())
+		return errors.NewProjectError(sourceFile, errors.ServiceError, err.Error())
 	}
 
 	for i, message := range resp.Messages {
 		msgContent, err := s.service.Users.Messages.Get(userId, message.Id).Do()
 		if err != nil {
-			return errors.NewProjectError("gmail_scrap_source.go", errors.ServiceError, err.Error())
+			return errors.NewProjectError(sourceFile, errors.ServiceError, err.Error())
 		}
+
 		for _, header := range msgContent.Payload.Headers {
-			if header.Name == "Subject" {
+			if header.Name == subject {
 				fmt.Println(i, header.Value)
 			}
 		}
